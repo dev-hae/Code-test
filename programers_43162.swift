2891b6971a01ba6cda90e95954478d7ae1a91e4d
hae <ninesunzero@gamil.com>
2024-07-15 22:36:43 +0900

    >>> 제목: 프로그래머스 - DFS - 43162

---
+++ programers_43162.swift
@@ -0,0 +1,30 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/43162
+//DFS로 풀엇당
+import Foundation
+
+var isVisted: [Bool] = Array(repeating: false, count: 200)
+func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+
+    var num = 0
+    for i in 0..<n {
+        if isVisted[i] == false {
+            num += 1
+            isVisted[i] = true
+            visiting(i, computers: computers, n: n)
+        }
+    }
+
+
+    return num
+}
+
+func visiting(_ i: Int, computers: [[Int]], n:Int) {
+
+    for j in 0..<n {
+        if isVisted[j] == false && computers[i][j] == 1 {
+            isVisted[j] = true
+            visiting(j, computers: computers, n: n)
+        }
+    }
+
+}
